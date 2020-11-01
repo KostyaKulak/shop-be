a92e497f9519669edea9993a8915d150c8e1702a
Kanstantsin Kulak <kkulak24@gmail.com>
2020-11-01 22:56:46 +0300

    feat: add async call

--- product-service/api/getProductsList.ts
+++ product-service/api/getProductsList.ts
@@ -1,8 +1,12 @@
 import {APIGatewayProxyHandler} from 'aws-lambda';
 import 'source-map-support/register';
 import {products} from "../resources/product/product.mocked";
+import fetch from 'node-fetch';
 
 export const getProductsList: APIGatewayProxyHandler = async () => {
+    const response = await fetch('http://worldtimeapi.org/api/ip')
+    const json = await response.json();
+    console.log(`Current time: ${json.datetime}`);
     return {
         statusCode: 200,
         body: JSON.stringify(products)
