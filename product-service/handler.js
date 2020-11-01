ca36b9ad8d9c7e0d9daff8d04f4de90be2bc9f38
Kanstantsin Kulak <kkulak24@gmail.com>
2020-11-01 19:45:54 +0300

    feat: update handler

--- product-service/handler.js
+++ product-service/handler.js
@@ -3,14 +3,11 @@
 module.exports.hello = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: {
+        title: 'Chanel Chance Eau Tendre',
+        description: 'Туалетная вода для женщин Шанель Шанс о Тендер.',
+        price: '208'
+    }
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
