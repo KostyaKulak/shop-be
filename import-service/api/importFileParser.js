3858249ae9d960aa4eff1c9115472c945132668c
Kanstantsin Kulak <kkulak24@gmail.com>
2020-11-15 23:23:04 +0300

    feat: implement moving file

--- import-service/api/importFileParser.js
+++ import-service/api/importFileParser.js
@@ -15,7 +15,21 @@ module.exports = {
             s3Stream.pipe(csv())
                 .on('data', (data) => console.log(data))
                 .on('error', (error) => reject(error))
-                .on('end', () => resolve());
+                .on('end', async () => {
+                    for (const record of event.Records) {
+                        await s3.copyObject({
+                            Bucket: AWS_S3_BUCKET,
+                            CopySource: AWS_S3_BUCKET + '/' + record.s3.object.key,
+                            Key: record.s3.object.key.replace('uploaded', 'parsed'),
+                        }).promise();
+
+                        await s3.deleteObject({
+                            Bucket: AWS_S3_BUCKET,
+                            Key: record.s3.object.key
+                        }).promise();
+                    }
+                    resolve();
+                });
         });
 
 
