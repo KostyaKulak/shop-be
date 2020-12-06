beb3f04bc842f78ad063304aaa28ac16c8c3f21a
Kanstantsin Kulak <kkulak24@gmail.com>
2020-12-07 02:10:37 +0300

    feat: update headers

--- core/headers.ts
+++ core/headers.ts
@@ -1,7 +1,4 @@
 export const headers = {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PATCH, PUT',
-    'Access-Control-Allow-Credentials': true
+    'Access-Control-Allow-Origin': '*'
 }
