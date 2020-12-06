40237af0278bdb9edd74a1d63c18a69fcf434af4
Kanstantsin Kulak <kkulak24@gmail.com>
2020-12-07 01:23:06 +0300

    feat: task 7

---
+++ authorization-service/serverless.ts
@@ -0,0 +1,35 @@
+import type { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+    service: {
+        name: 'authorization-service-be',
+    },
+    frameworkVersion: '2',
+    custom: {
+        webpack: {
+            webpackConfig: './webpack.config.js',
+            includeModules: true,
+        },
+    },
+    plugins: ['serverless-webpack', 'serverless-dotenv-plugin'],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs12.x',
+        region: 'eu-west-1',
+        apiGateway: {
+            minimumCompressionSize: 1024,
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+            LOGIN: process.env.LOGIN,
+            PASSWORD: process.env.PASSWORD,
+        },
+    },
+    functions: {
+        basicAuthorizer: {
+            handler: 'handler.basicAuthorizer',
+        },
+    },
+};
+
+module.exports = serverlessConfiguration;
--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,10 +1,10 @@
-import type {Serverless} from 'serverless/aws';
-import {AWS_S3_BUCKET} from "./constants/common";
-import {AWS_REGION, AWS_SQS_QUEUE, AWS_STACK_ID} from "../core/constants";
+import type { Serverless } from 'serverless/aws';
+import { AWS_S3_BUCKET } from './constants/common';
+import { AWS_REGION, AWS_SQS_QUEUE, AWS_STACK_ID } from '../core/constants';
 
 const serverlessConfiguration: Serverless = {
     service: {
-        name: 'import-service'
+        name: 'import-service',
     },
     frameworkVersion: '2',
     custom: {
@@ -12,8 +12,8 @@ const serverlessConfiguration: Serverless = {
             webpackConfig: './webpack.config.js',
             includeModules: {
                 forceExclude: 'aws-sdk',
-            }
-        }
+            },
+        },
     },
     plugins: ['serverless-webpack'],
     provider: {
@@ -26,13 +26,17 @@ const serverlessConfiguration: Serverless = {
         },
         environment: {
             AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-            SQS_URL: `https://sqs.${AWS_REGION}.amazonaws.com/${AWS_STACK_ID}/${AWS_SQS_QUEUE}`
+            SQS_URL: `https://sqs.${AWS_REGION}.amazonaws.com/${AWS_STACK_ID}/${AWS_SQS_QUEUE}`,
         },
         iamRoleStatements: [
-            {Effect: 'Allow', Action: 's3:ListBucket', Resource: [`arn:aws:s3:::${AWS_S3_BUCKET}`]},
-            {Effect: 'Allow', Action: 's3:*', Resource: [`arn:aws:s3:::${AWS_S3_BUCKET}/*`]},
-            {Effect: 'Allow', Action: 'sqs:*', Resource: [`arn:aws:sqs:${AWS_REGION}:${AWS_STACK_ID}:${AWS_SQS_QUEUE}`]}
-        ]
+            { Effect: 'Allow', Action: 's3:ListBucket', Resource: [`arn:aws:s3:::${AWS_S3_BUCKET}`] },
+            { Effect: 'Allow', Action: 's3:*', Resource: [`arn:aws:s3:::${AWS_S3_BUCKET}/*`] },
+            {
+                Effect: 'Allow',
+                Action: 'sqs:*',
+                Resource: [`arn:aws:sqs:${AWS_REGION}:${AWS_STACK_ID}:${AWS_SQS_QUEUE}`],
+            },
+        ],
     },
     functions: {
         importProductsFile: {
@@ -46,13 +50,21 @@ const serverlessConfiguration: Serverless = {
                         request: {
                             parameters: {
                                 querystrings: {
-                                    name: true
-                                }
-                            }
-                        }
-                    }
-                }
-            ]
+                                    name: true,
+                                },
+                            },
+                        },
+                        authorizer: {
+                            name: 'TokenAuthorizer',
+                            arn:
+                                `arn:aws:lambda:${AWS_REGION}:${AWS_STACK_ID}:function:authorization-service-be-dev-basicAuthorizer`,
+                            resultTtlInSeconds: 0,
+                            identitySource: 'method.request.header.Authorization',
+                            type: 'token',
+                        },
+                    },
+                },
+            ],
         },
         importFileParser: {
             handler: 'api/importFileParser.importFileParser',
@@ -62,14 +74,45 @@ const serverlessConfiguration: Serverless = {
                         bucket: AWS_S3_BUCKET,
                         event: 's3:ObjectCreated:*',
                         rules: [
-                            {prefix: 'uploaded/', suffix: ''}
+                            { prefix: 'uploaded/', suffix: '' },
                         ],
-                        existing: true
-                    }
-                }
-            ]
-        }
-    }
-}
+                        existing: true,
+                    },
+                },
+            ],
+        },
+    },
+    resources: {
+        Resources: {
+            GatewayResponseAccessDenied: {
+                Type: 'AWS::ApiGateway::GatewayResponse',
+                Properties: {
+                    RestApiId: {
+                        Ref: 'ApiGatewayRestApi',
+                    },
+                    ResponseType: 'ACCESS_DENIED',
+                    ResponseParameters: {
+                        'gatewayresponse.header.Access-Control-Allow-Origin': '\'*\'',
+                        'gatewayresponse.header.Access-Control-Allow-Headers': '\'*\'',
+                    },
+                },
+            },
+
+            GatewayResponseUnauthorized: {
+                Type: 'AWS::ApiGateway::GatewayResponse',
+                Properties: {
+                    RestApiId: {
+                        Ref: 'ApiGatewayRestApi',
+                    },
+                    ResponseType: 'UNAUTHORIZED',
+                    ResponseParameters: {
+                        'gatewayresponse.header.Access-Control-Allow-Origin': '\'*\'',
+                        'gatewayresponse.header.Access-Control-Allow-Headers': '\'*\'',
+                    },
+                },
+            },
+        },
+    },
+};
 
 module.exports = serverlessConfiguration;
