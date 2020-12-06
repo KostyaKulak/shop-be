3bf62cd57a5d633b8580f1364a139eab3441984f
Kanstantsin Kulak <kkulak24@gmail.com>
2020-12-07 00:46:26 +0300

    feat: add jest

---
+++ product-service/interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface NewProductRecord {
+    title: string;
+    description: string;
+    price: number;
+    count: number;
+}
+
+export interface ProductWithStockRecord {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    count: number;
+}
